4050416c70acbab91117a1c5350ec9f9e8431fff
Felicity Tarnell <ft@le-Fay.ORG>
2016-06-03 03:53:15 +0100

    use postgres in production

--- vaper/settings/production.py
+++ vaper/settings/production.py
@@ -2,4 +2,11 @@ from .base import *
 
 ALLOWED_HOSTS = [ 'vaper.le-fay.org' ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vaper',
+    }
+}
+
 from .local import *
