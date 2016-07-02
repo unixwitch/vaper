7348fc846eec916177f58d8719e31214de020910
Felicity Tarnell <ft@le-Fay.ORG>
2016-07-02 16:05:04 +0100

    remove django admin from apps

--- vaper/settings/base.py
+++ vaper/settings/base.py
@@ -14,7 +14,6 @@ INSTALLED_APPS = [
     'datetimewidget',
     'widget_tweaks',
 
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
